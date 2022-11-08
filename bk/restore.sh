d6173f1936db7f4048a4f5921437df66a212ab31
AikoCute <aikocute@icloud.com>
2022-11-08 16:49:14 +0700

    Update

--- bk/restore.sh
+++ bk/restore.sh
@@ -7,6 +7,6 @@
 
    
  /opt/mssql-tools/bin/sqlcmd \
--S localhost -U SA -P 'KienMLOK' \
+-S localhost -U SA -P 'Password123' \
 -Q 'RESTORE DATABASE xtlab FROM DISK = "/var/opt/mssql/backup/xtlab.bak" WITH MOVE "xtlab" TO "/var/opt/mssql/data/xtlab.mdf", MOVE "xtlab_log" TO "/var/opt/mssql/data/xtlab_log.ldf"'
 
